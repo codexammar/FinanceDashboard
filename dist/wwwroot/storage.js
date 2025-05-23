74c36f19b3db06eab51a059c49619b47ad7e340e
AmmarSchool <syed.ammar.ali@torontomu.ca>
2025-05-23 16:11:14 -0400

    Final clean deploy to gh-pages

--- dist/wwwroot/storage.js
+++
@@ -1,9 +0,0 @@
-window.transactionStorage = {
-    save: function (key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
-    },
-    load: function (key) {
-        const value = localStorage.getItem(key);
-        return value ? JSON.parse(value) : [];
-    }
-};
